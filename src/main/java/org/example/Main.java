8326bc27bb903fbe3d193a804aa5b1029d416e08
Ugina <sleepyboy906sb@gmail.com>
2022-10-15 02:00:36 +0300

    Add finding right table in page

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
+import static org.example.Parser.getPage;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Document page = getPage();
+        //System.out.println(page);
+        Element tableWeather = page.select("div[id=jm-content-top]").first();
+        System.out.println(tableWeather + " HERE");
     }
 }
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -1,5 +1,17 @@
 package org.example;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Parser {
-    
+    public static Document getPage() throws IOException {
+        String url = "https://meteoinfo.ru/";
+        Document page = Jsoup.parse(new URL(url), 3000);
+        return page;
+    }
+
 }
