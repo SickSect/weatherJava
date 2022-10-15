d3a8c948c500d303537636cb0057ddaa46d7de22
Ugina <sleepyboy906sb@gmail.com>
2022-10-15 21:50:35 +0300

    add methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,30 @@ package org.example;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 import static org.example.Parser.getPage;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         Document page = getPage();
         //System.out.println(page);
-        Element tableWeather = page.select("div[id=jm-content-top]").first();
-        System.out.println(tableWeather + " HERE");
+        Parser pars = new Parser();
+        Element tableWeather = page.select("div[class=current-weather-wrap]").first();
+        Element date = tableWeather.select("div[class=current-time]").first();
+        Element wind = tableWeather.select("div[class=weather-item weather-wind]").first();
+        Element pressure = tableWeather.select("div[class=weather-item weather-pressure]").first();
+        Element temperature = page.getElementsByClass("unit unit_temperature_c").first();
+        Element event = tableWeather.select("div[class=weather-description]").first();
+        //String date = "";
+
+        System.out.println(pars.getDate(date.text()));
+        System.out.println(pars.getWind(wind.text()));
+        System.out.println(pars.getTemp(temperature.text()));
+        System.out.println(pars.getPressure(pressure.text()));
+        System.out.println(event.text());
     }
 }
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -2,16 +2,60 @@ package org.example;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Parser {
+
+    String getDate(String dateStr) throws Exception {
+        //\d{2}\s\D{3, 13} \d{2}\s\w{3,10}
+        Pattern pattern = Pattern.compile("\\d{2}\\s\\X+[,]");
+        Matcher match = pattern.matcher(dateStr);
+        //Сб, 15 октября, 17:40
+        if(match.find()) {
+            String date = match.group();
+            return date.substring(0,date.length() - 1);
+        }
+        throw new Exception("Cant extract date from string in method Parser.getDate");
+    }
     public static Document getPage() throws IOException {
-        String url = "https://meteoinfo.ru/";
+        String url = "https://www.gismeteo.ru/";
         Document page = Jsoup.parse(new URL(url), 3000);
         return page;
     }
 
+    public String getWind(String windStr) throws Exception {
+        Pattern pattern = Pattern.compile("\\D{5}\\s\\d{1,2}");
+        Matcher match  = pattern.matcher((windStr));
+        if(match.find()){
+            String wind = match.group() + " м/с";
+            return wind;
+        }
+        //return windStr;
+        throw new Exception("Cant extract wind from string in method getWind");
+    }
+
+    public String getTemp(String tempStr) throws Exception{
+        Pattern pattern = Pattern.compile("[+-]\\d{1,2}");
+        Matcher match = pattern.matcher(tempStr);
+        if(match.find())
+        {
+            return "Температура " + match.group() + " C";
+        }
+        throw new Exception("Cant extract temperature from string in method getTemp");
+    }
+    //Давление 752 мм рт.ст. 1002 гПа
+    public String getPressure(String pressStr) throws Exception{
+        //Давление 752 мм рт.ст. 1002 гПа
+        Pattern pattern = Pattern.compile("\\D{8}\\s\\d{3}\\s");
+        Matcher match = pattern.matcher(pressStr);
+        if (match.find())
+            return match.group() + "мм рт.ст.";
+        throw new Exception("Cant extract pressure from string in method getTemp");
+    }
 }
